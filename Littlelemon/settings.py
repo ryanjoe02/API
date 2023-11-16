095fea1e6b8d1cf80e5b5a5fddd784c6b316b8d3
syjoe <csy5501@gmail.com>
2023-11-16 16:23:05 +0900

    use renderer globally

--- Littlelemon/settings.py
+++ Littlelemon/settings.py
@@ -132,3 +132,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# To make these renderers available globally
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+        'rest_framework_csv.renderers.CSVRenderer', 
+        'rest_framework_yaml.renderers.YAMLRenderer', 
+    ]
+}
