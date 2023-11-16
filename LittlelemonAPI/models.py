7aad827d9898a31d1eed1007c0cab0fb87e5a380
syjoe <csy5501@gmail.com>
2023-11-16 13:50:39 +0900

    restaurant model

--- LittlelemonAPI/models.py
+++ LittlelemonAPI/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+
+    # for string converstion
+    def __str__(self) -> str:
+        return self.title
+
+
 class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
-        
+    # category cannot be deleted before all the related menu items are deleted
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
