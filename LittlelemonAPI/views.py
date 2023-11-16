49422b5deecad87d071cb4be2b75e9ee278f2dd5
syjoe <csy5501@gmail.com>
2023-11-16 13:51:36 +0900

    menu & single menu items

--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+
+
+# /api/menu-item/ : display the menu items that you've set in serializers.py as JSON data
+
+@api_view(['GET', 'POST'])
+def menu_item(request):
+    items = MenuItem.objects.select_related("category").all()
+    serialized_item = MenuItemSerializer(items, many=True)
+    return Response(serialized_item.data)
+
+@api_view(['GET', 'POST'])
+def single_item(request):
+    items = get_object_or_404(MenuItem, pk=id)
+    serialized_item = MenuItemSerializer(items)
+    return Response(serialized_item.data)
