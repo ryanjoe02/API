f2dd53d79679c13aa1192d6e5b9f18a7a69cf127
syjoe <csy5501@gmail.com>
2023-11-18 11:56:15 +0900

    add pagination

--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage
 
 from .models import MenuItem
 from .serializers import MenuItemSerializer
@@ -15,6 +16,28 @@ from .serializers import MenuItemSerializer
 def menu_item(request):
     if request.method == "GET":
         items = MenuItem.objects.select_related("category").all()
+        category_name = request.query_params.get("category")
+        to_price = request.query_params.get("to_price")
+        search = request.query_params.get("search")
+        ordering = request.query_params.get("ordering")
+        # Pagination
+        perpage = request.query_params.get("perpage", default=2)
+        page = request.query_params.get("page", default=1)
+        if category_name:
+            items = items.filter(category__title=category_name)
+        if to_price:
+            items = items.filter(price__lte=to_price)
+        if search:
+            items = items.filter(title__istartwith=search)
+        if ordering:
+            ordering_fields = ordering.split(",")
+            items = items.order_by(*ordering_fields)
+
+        paginator = Paginator(items, per_page=perpage)
+        try:
+            items = paginator.page(number=page)
+        except EmptyPage:
+            items = []
         serialized_item = MenuItemSerializer(items, many=True)
         return Response(serialized_item.data)
     if request.method == "POST":
