2005ec69e8b4e35dde83dac30d36789874c73dc9
syjoe <csy5501@gmail.com>
2023-11-18 11:56:02 +0900

    add blench

--- LittlelemonAPI/serializers.py
+++ LittlelemonAPI/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from decimal import Decimal
+import bleach
 
 from .models import MenuItem, Category
 
@@ -18,15 +19,30 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class MenuItemSerializer(serializers.ModelSerializer):
     # can change the field name
-    stock = serializers.IntegerField(source="inventory")
     price_after_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=2)
     # relationship with serializers
     category = CategorySerializer(read_only=True)
-    category_id = serializers.IntegerField()
+    category_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = MenuItem
-        fields = ["id", "title", "price", "stock", "price_after_tax", "category"]
+        fields = [
+            "id",
+            "title",
+            "price",
+            "stock",
+            "price_after_tax",
+            "category",
+            "category_id",
+        ]
+        extra_kwargs = {
+            "price": {"min_value": 2},
+            "stock": {"source": "inventory", "min_value": 0}
+        }
 
     def calculate_tax(self, product: MenuItem):
         return product.price * Decimal(1.1)
+
+    def validate_title(self, value):
+        return bleach.clean(value)
