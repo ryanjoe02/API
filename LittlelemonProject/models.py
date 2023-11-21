a961f6d98c33b67821a1287e94b522cc4dde3318
syjoe <csy5501@gmail.com>
2023-11-21 19:14:45 +0900

    apply black format

--- LittlelemonProject/models.py
+++ LittlelemonProject/models.py
@@ -6,11 +6,13 @@ class Category(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=255, db_index=True)
 
+
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(db_index=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,14 +22,18 @@ class Cart(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     class Meta:
-        unique_together = ('user', 'menuitem')
+        unique_together = ("user", "menuitem")
+
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
+    delivery_crew = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True
+    )
     status = models.BooleanField(db_index=True, default=0)
     total = models.DecimalField(max_length=6, decimal_places=2)
     date = models.DateField(db_index=True)
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -37,4 +43,4 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_length=6, decimal_places=2)
 
     class Meta:
-        unique_together = ('order', 'menuitem')
+        unique_together = ("order", "menuitem")
