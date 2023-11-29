cd26e15a8f7ea4bb09028a9926f6acd482362c9a
syjoe <csy5501@gmail.com>
2023-11-29 21:07:41 +0900

    add command that I don't use this code now

--- Littlelemon/urls.py
+++ Littlelemon/urls.py
@@ -27,9 +27,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("LittlelemonAPI.urls")),
     # Djoser
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.authtoken")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
+    # path("auth/", include("djoser.urls")),
+    # path("auth/", include("djoser.urls.authtoken")),
+    # path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # path("api/token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
 ]
