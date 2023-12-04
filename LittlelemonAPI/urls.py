81ad8fd26da4c3dd2c45a559afa3c4fbade28a79
syjoe <csy5501@gmail.com>
2023-12-04 16:56:14 +0900

    make Cart API

--- LittlelemonAPI/urls.py
+++ LittlelemonAPI/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 # from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework import routers
 
-from .views import CategoryViewSet, MenuItemViewSet
+from .views import CategoryViewSet, MenuItemViewSet, CartViewSet
 
 
 router = routers.DefaultRouter()
+router.register("cart", CartViewSet, basename="cart")
 router.register("categories", CategoryViewSet)
 router.register("menu-items", MenuItemViewSet)
 
--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.permissions import IsAdminUser, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly, IsAuthenticated
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 
-from .models import MenuItem, Category
-from .serializers import MenuItemSerializer, CategorySerializer
+from .models import MenuItem, Category, Cart
+from .serializers import MenuItemSerializer, CategorySerializer, UpdateCartSerializer, AddCartSerializer, CartSerializer
 from .filters import MenuItemFilter
 from .paginations import MenuItemPagnation
 
@@ -29,3 +29,28 @@ class MenuItemViewSet(viewsets.ModelViewSet):
     pagination_class = MenuItemPagnation
     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     throttle_classes = [UserRateThrottle, AnonRateThrottle]
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    http_method_names = ["get", "post", "patch", "delete", "head", "options"]
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return AddCartSerializer
+        elif self.request.method == "PATCH":
+            return UpdateCartSerializer
+        return CartSerializer
+
+    def get_serializer_context(self):
+        return {"user_id": self.request.user.id}
+
+    def get_queryset(self):
+        user = self.request.user.id
+
+        try:
+            return Cart.objects.filter(user_id=user).select_related("menuitem").all()
+        except Cart.DoesNotExist:
+            return Cart.objects.create(user_id=user)
+    
